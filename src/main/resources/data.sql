98624210b9fb787ceead69b1417920b6e07ebf38
chae <luxury515@naver.com>
2022-09-28 16:40:55 +0900

    re commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO `user` (`username`, `email`) VALUES ('cai1', 'cai1@gmail.com');
+INSERT INTO `user` (`username`, `email`) VALUES ('cai2', 'cai2@gmail.com');
+INSERT INTO `user` (`username`, `email`) VALUES ('cai3', 'cai3@gmail.com');
+INSERT INTO `user` (`username`, `email`) VALUES ('cai4', 'cai4@gmail.com');
+INSERT INTO `user` (`username`, `email`) VALUES ('cai5', 'cai5@gmail.com');
+INSERT INTO `user` (`username`, `email`) VALUES ('cai6', 'cai6@gmail.com');
+INSERT INTO `user` (`username`, `email`) VALUES ('cai7', 'cai7@gmail.com');
+INSERT INTO `user` (`username`, `email`) VALUES ('cai8', 'cai8@gmail.com');
