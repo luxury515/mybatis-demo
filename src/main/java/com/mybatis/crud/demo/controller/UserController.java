e96f9e41402b6c95e24c1413c7bcf2af667f3e5d
chae <luxury515@naver.com>
2022-09-28 16:39:59 +0900

    add data.sql ,others default setting change

--- src/main/java/com/mybatis/crud/demo/controller/UserController.java
+++ src/main/java/com/mybatis/crud/demo/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +39,7 @@ public class UserController {
     return new ResponseEntity<>(user, HttpStatus.OK);
   }
 
-  @PostMapping("/user/update/{id}")
+  @PutMapping("/user/update/{id}")
   public ResponseEntity<String> getUser(@PathVariable Long id,@RequestBody User user) {
     int resultCode = userService.updateUser(id,user);
     if (resultCode != 1) {
